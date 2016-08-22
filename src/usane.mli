298a145764e854fe513d514a29d21b8817264407
Hannes Mehnert <hannes@mehnert.org>
2016-08-23 00:18:51 +0100

    doc

--- src/usane.mli
+++ src/usane.mli
@@ -1,5 +1,23 @@
 (** Sane unsigned integers.
 
+    Arithmetic operations on unsigned integers of fixed width may overflow.
+    This library exposes the carry bit of arithmetic operations explicitly: The
+    value of each arithmetic operation is a tuple of the (maybe wrapped) value,
+    and the carry bit.
+
+    It is designed for network protocols where sequence numbers are used (and
+    either wrapping around is accepted or should lead to re-establishing or
+    re-keying of the connection).  Most protocols only use simple arithmetic
+    operations, such as a comparison and incrementing by one.
+
+    To ease interoperation with other libraries, the representation is the same
+    width signed representation provided by OCaml, if present (which is the case
+    for [int32] and [int64]).  Watch out to avoid [Int32] and [Int64] operations
+    which wrap around silently.
+
+    The implementation of [usane] uses C compiler builtins
+    (__builtin_uadd_overflow etc.).  These are available since GCC 5.1 and Clang
+    3.4.
 
     {e %%VERSION%% - {{:%%PKG_HOMEPAGE%% }homepage}} *)
 
@@ -12,35 +30,36 @@ module Uint32 : sig
   (** [pp ppf u] prints the unsigned 32bit integer in hex encoding. *)
   val pp : Format.formatter -> t -> unit
 
-  (** [zero] is 0. *)
+  (** [zero] is [0]. *)
   val zero : t
 
-  (** [one] is 1. *)
+  (** [one] is [1]. *)
   val one : t
 
   (** [of_int i] is the integer [i] converted to an unsigned 32 bit integer.
       @raise Invalid_argument if the integer is out of range. *)
   val of_int : int -> t
 
-  (** [to_int t] is the integer representation of [t], if it fits, [None]
-      otherwise. *)
+  (** [to_int t] is the integer representation of [t], encapsulated in [Some].
+      If the value [t] does not fit on the host system (if it is 32 bit or
+      smaller), it is [None]. *)
   val to_int : t -> int option
 
-  (** [add t t'] is [r, wrap], where [r] is the result of [t + t' mod (2 ^ 32 -
-      1)].  If the sum does not fit into 32 bits, [wrap] is [true], otherwise
+  (** [add t t'] is [(r, carry)], where [r] is [t + t' mod (2 ^ 32 - 1)].  If
+      the sum does not fit into 32 bits, [carry] is [true], otherwise it is
       [false]. *)
   val add : t -> t -> t * bool
 
-  (** [sub t t'] is [r, wrap], where [r] is the result of [t - t' mod (2 ^ 32 -
-      1)].  If [t] is smaller than [t'], [wrap] is [true], otherwise [false]. *)
+  (** [sub t t'] is [(r, carry)], where [r] is [t - t' mod (2 ^ 32 - 1)].  If
+      [t] is smaller than [t'], [carry] is [true], otherwise it is [false]. *)
   val sub : t -> t -> t * bool
 
-  (** [succ t] is the successor of [t]: [add t one].  If [t] is 2 ^ 32 - 1,
-      [wrap] is [true], otherwise [false]. *)
+  (** [succ t] is the successor of [t]: [add t one].  If [t] is [2 ^ 32 - 1],
+      [carry] is [true], otherwise it is [false]. *)
   val succ : t -> t * bool
 
-  (** [pred t] is the predecessor of [t]: [sub t one].  If [t] is [zero], [wrap]
-      is [true], otherwise [false]. *)
+  (** [pred t] is the predecessor of [t]: [sub t one].  If [t] is [0], [carry]
+      is [true], otherwise it is [false]. *)
   val pred : t -> t * bool
 
   (** [compare t t'] is
